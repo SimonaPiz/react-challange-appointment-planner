6a235251586dca05f0c76ff564a92d405a0f3284
Simona Pizio <simona.pizio@gmail.com>
2022-03-24 11:13:23 +0100

    Resolve task 2, form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,28 +9,42 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+  const handleChangeName = ({target}) => setName(target.value);
+  const handleChangePhone = ({target}) => setPhone(target.value);
+  const handleChangeEmail = ({target}) => setEmail(target.value);
+
   return (
     <form onSubmit={handleSubmit}>
-      <label for='username'>User Name:</label>
+      <label for='username'>enter your Name:</label>
       <input 
         value={name}
         type='text'
         name="username"
         id="username"
+        onChange={handleChangeName}
+        placeholder='name'
+        required
       />
-      <label for='phone'>Phone Number:</label>
+      <label for='phone'>enter your Phone Number:</label>
       <input 
         value={phone}
         type='tel'
         name="phone"
         id="phone"
+        onChange={handleChangePhone}
+        placeholder='000-000000'
+        pattern="[0-9]{3}-[0-9]{6}"
+        required
       />
-      <label for='email'>User email:</label>
+      <label for='email'>enter your Email:</label>
       <input 
         value={email}
         type='email'
         name="email"
         id="email"
+        onChange={handleChangeEmail}
+        placeholder='email@'
+        required
       />
       <input type='submit' value='Submit'/>
     </form>
