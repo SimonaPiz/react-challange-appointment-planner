2fac89047526b6931ed70bb3a0c924e8482cb727
Simona Pizio <simona.pizio@gmail.com>
2022-03-24 15:53:11 +0100

    task 3 and 4, Tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -3,9 +3,9 @@ import React from "react";
 export const Tile = ({contact}) => {
   return (
     <div className="tile-container">
-      <p>{contact.name}</p>
-      <p>{contact.phone}</p>
-      <p>{contact.email}</p>
+      <p className="tile-title">{contact.name}</p>
+      <p className="tile">{contact.phone}</p>
+      <p className="tile">{contact.email}</p>
     </div>
   );
 };
