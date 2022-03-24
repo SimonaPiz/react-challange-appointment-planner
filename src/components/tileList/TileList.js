c17385476cdfa6513b60c91733fc453d675c91c8
Simona Pizio <simona.pizio@gmail.com>
2022-03-24 11:32:54 +0100

    Resolved issue 5, task 1 and 2

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({contacts}) => {
   return (
     <div>
-      
+      {contacts.map(contact => {
+        <Tile contact={contact}/>
+      })}
     </div>
   );
 };
